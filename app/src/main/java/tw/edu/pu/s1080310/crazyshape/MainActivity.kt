c559b5285bb34ed320da195b36f20d2ce4a9d833
tzuyu <s1080310@gm.pu.edu.tw>
2021-05-18 12:18:35 +0800

    第一次commit

---
+++ app/src/main/java/tw/edu/pu/s1080310/crazyshape/MainActivity.kt
@@ -0,0 +1,29 @@
+package tw.edu.pu.s1080310.crazyshape
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        Toast.makeText(baseContext, "作者：王姿侑", Toast.LENGTH_LONG).show()
+
+
+
+        imgNext.setOnLongClickListener(object : View.OnLongClickListener {
+            override fun onLongClick(p0: View?): Boolean {
+                intent = Intent(this@MainActivity, GameActivity::class.java)
+                startActivity(intent)
+                return true
+            }
+        })
+    }
+}
