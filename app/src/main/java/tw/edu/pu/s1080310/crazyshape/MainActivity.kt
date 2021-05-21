2f67f52f435ffcb987288d7628d0fd167b81e4a9
tzuyu <s1080310@gm.pu.edu.tw>
2021-05-21 14:50:25 +0800

    2021上機考v2_(王姿侑)

--- app/src/main/java/tw/edu/pu/s1080310/crazyshape/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/s1080310/crazyshape/MainActivity.kt
@@ -3,15 +3,32 @@ package tw.edu.pu.s1080310.crazyshape
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MotionEvent
 import android.view.View
+import android.widget.ImageView
 import android.widget.Toast
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
 import kotlinx.android.synthetic.main.activity_main.*
+
+
+@GlideModule
+public final class MyAppGlideModule : AppGlideModule()
+
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val img: ImageView = findViewById(R.id.imgTitle)
+        MyAppGlideModule.with(this)
+            .load(R.drawable.cover)
+            .override(800, 600)
+            .into(img)
+
+
 
 
         Toast.makeText(baseContext, "作者： 王姿侑 ", Toast.LENGTH_LONG).show()
